09118c2116e50eae5f7d75f2c555863b6eba608c
sugz <649114961@qq.com>
2020-05-13 17:50:52 +0800

    update

--- sugz/src/main/java/com/android/sgzcommon/utils/FileUtil.java
+++ sugz/src/main/java/com/android/sgzcommon/utils/FileUtil.java
@@ -1,19 +1,12 @@
 package com.android.sgzcommon.utils;
 
-import android.content.ContentUris;
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
-import android.provider.OpenableColumns;
 import android.text.TextUtils;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 /**
  * Created by sgz on 2016/a12/20.
@@ -63,152 +56,48 @@ public class FileUtil {
      * @return
      */
     public static boolean deleteFile(File file) {
-        if (file.isFile()) {
-            return file.delete();
-        }
-        if (file.isDirectory()) {
-            File[] childFiles = file.listFiles();
-            if (childFiles == null || childFiles.length == 0) {
+        try {
+            if (file.isFile()) {
                 return file.delete();
             }
-            for (int i = 0; i < childFiles.length; i++) {
-                deleteFile(childFiles[i]);
+            if (file.isDirectory()) {
+                File[] childFiles = file.listFiles();
+                if (childFiles == null || childFiles.length == 0) {
+                    return file.delete();
+                }
+                for (int i = 0; i < childFiles.length; i++) {
+                    deleteFile(childFiles[i]);
+                }
+                return file.delete();
             }
-            return file.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return false;
     }
 
-
     /**
-     * 把uri转换成文件路径
+     * 文件复制
+     * @param source 源文件
+     * @param dest
+     * @return
+     * @throws IOException
      */
-    public static String getPathFromUri(final Context context, final Uri uri) throws Exception {
-        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
-        final boolean isN = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N;
-        if (isN) {
-            return getFilePathForN(context, uri);
-        }
-        // DocumentProvider
-        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-            // ExternalStorageProvider
-            if (isExternalStorageDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                if ("primary".equalsIgnoreCase(type)) {
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                }
-
-            }
-            // DownloadsProvider
-            else if (isDownloadsDocument(uri)) {
-
-                final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-
-                return getDataColumn(context, contentUri, null, null);
-            }
-            // MediaProvider
-            else if (isMediaDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                Uri contentUri = null;
-                if ("image".equals(type)) {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                } else if ("video".equals(type)) {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                } else if ("audio".equals(type)) {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                }
-
-                final String selection = "_id=?";
-                final String[] selectionArgs = new String[]{split[1]};
-
-                return getDataColumn(context, contentUri, selection, selectionArgs);
-            }
-        }
-        // MediaStore (and general)
-        else if ("content".equalsIgnoreCase(uri.getScheme())) {
-            return getDataColumn(context, uri, null, null);
-        }
-        // File
-        else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            return uri.getPath();
-        }
-
-        return null;
-    }
-
-    /**
-     * android7.0以上处理方法
-     */
-    private static String getFilePathForN(Context context, Uri uri) {
+    public static boolean copyFile(File source, File dest) throws IOException {
+        boolean result = false;
+        FileChannel inputChannel = null;
+        FileChannel outputChannel = null;
         try {
-            Cursor returnCursor = context.getContentResolver().query(uri, null, null, null, null);
-            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-            returnCursor.moveToFirst();
-            String name = (returnCursor.getString(nameIndex));
-            File file = new File(context.getFilesDir(), name);
-            InputStream inputStream = context.getContentResolver().openInputStream(uri);
-            FileOutputStream outputStream = new FileOutputStream(file);
-            int read = 0;
-            int maxBufferSize = 1 * 1024 * 1024;
-            int bytesAvailable = inputStream.available();
-
-            int bufferSize = Math.min(bytesAvailable, maxBufferSize);
-
-            final byte[] buffers = new byte[bufferSize];
-            while ((read = inputStream.read(buffers)) != -1) {
-                outputStream.write(buffers, 0, read);
-            }
-            returnCursor.close();
-            inputStream.close();
-            outputStream.close();
-            return file.getPath();
+            inputChannel = new FileInputStream(source).getChannel();
+            outputChannel = new FileOutputStream(dest).getChannel();
+            outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
+            result = true;
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * 获取此Uri的数据列的值。这对于MediaStore uri和其他基于文件的内容提供程序非常有用。
-     */
-    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
-
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {column};
-
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int column_index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(column_index);
-            }
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
         } finally {
-            if (cursor != null)
-                cursor.close();
+            inputChannel.close();
+            outputChannel.close();
         }
-        return null;
+        return result;
     }
-
-    public static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    public static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    public static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
 }
